147ee9fb7ab26b4977177d10ea6442e00fd0d159
codinSamueldev <codinsamueldev@gmail.com>
2025-05-18 16:03:05 -0500

    Update project dependencies and database settings
    - Added new dependencies for asyncpg, psycopg with connection pooling, and python-decouple to enhance database performance and configuration management.
    - Updated the DATABASE_URL handling in settings.py to utilize python-decouple for improved environment variable management.
    - Enhanced database configuration to include connection health checks and maximum age settings for better performance.
    - Updated lock file to reflect the new package versions and ensure consistent installations across environments.

--- lazy_prompts/lazy_prompts/settings.py
+++ lazy_prompts/lazy_prompts/settings.py
@@ -12,9 +12,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
+from urllib.parse import urlparse
 
 from dotenv import load_dotenv
 import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,11 +88,18 @@ WSGI_APPLICATION = 'lazy_prompts.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASE_URL = config('DATABASE_URL', cast=str)
 
-DATABASES = {
-    'default': dj_database_url.parse(DATABASE_URL)
-}
+print(f"\nDB_URL: {DATABASE_URL}\n")
+
+if DATABASE_URL is not None:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=30,
+            conn_health_checks=True
+        )
+    }
 
 
 # Password validation
