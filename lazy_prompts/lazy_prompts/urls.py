79a0f380c460617715d5444e590fa62f20e86adb
codinSamueldev <codinsamueldev@gmail.com>
2025-05-22 16:54:28 -0500

    Add namespacing to feed URLs for improved organization
    - Added app_name to feed/urls.py to facilitate namespacing of the feed app's URL patterns.
    - Updated the main project urls.py to include the feed URLs with a namespace, enhancing clarity and modularity in routing.
    - This change improves the structure of URL configurations, making it easier to manage and reference feed-related routes.

--- lazy_prompts/lazy_prompts/urls.py
+++ lazy_prompts/lazy_prompts/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('feed.urls')),
+    path('', include('feed.urls', namespace='feed')),
     path('u/', include('users.urls', namespace='users')),
     path('accounts/', include('allauth.urls')),
 ]
