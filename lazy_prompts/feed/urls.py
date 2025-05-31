6b843be1747191f4fb82864e0b16d92c9c2aa221
codinSamueldev <codinsamueldev@gmail.com>
2025-05-31 17:00:51 -0500

    feat: add infinite scroll with pagination each 3 posts
    - new endpoint to handle asynchronous pagination
    - Create partials folder to load posts

--- lazy_prompts/feed/urls.py
+++ lazy_prompts/feed/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from .views import home
+from .views import home, load_more_posts
 
 
 app_name = 'feed'
 
 urlpatterns = [
     path('', home, name="home"),
+    path('load-more/', load_more_posts, name="load_more_posts")
 ]
 
 
--- lazy_prompts/feed/views.py
+++ lazy_prompts/feed/views.py
@@ -1,10 +1,46 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.http import JsonResponse
+from django.template.loader import render_to_string
+
+
 from prompt_posts.models import Prompt
 
 
 def home(request):
     prompts = Prompt.objects.all()
-    return render(request, 'home.html', {'prompts': prompts})
+
+    paginator = Paginator(prompts, 3) # 3 Posts per page.
+    page_1 = paginator.get_page(1) # Initial load with 3 posts.
+
+    context = {
+        'prompts': page_1,
+        'has_next': page_1.has_next(),
+        'next_page_number': page_1.next_page_number() if page_1.has_next() else None,
+    }
 
 
+    return render(request, 'home.html', context)
 
+
+def load_more_posts(request):
+    """
+    AJAX endpoint to load more posts.
+    """
+    if request.headers.get("X-Requested-With") == "XMLHttpRequest":
+        page_number = request.GET.get("page", 1)
+
+        prompts = Prompt.objects.all()
+        paginator = Paginator(prompts, 3)
+        page = paginator.get_page(page_number)
+
+        # Render just the posts HTML
+        posts_html = render_to_string('partials/posts.html', context={'prompts': page}, request=request)
+
+        return JsonResponse({
+            'posts_html': posts_html,
+            'has_next': page.has_next(),
+            'next_page_number': page.next_page_number() if page.has_next() else None,
+        })
+
+    return JsonResponse({"error": "Invalid request"}, status=400)
