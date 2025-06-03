0226dc1ae00cc6384ef308099ef691bc38796df7
codinSamueldev <codinsamueldev@gmail.com>
2025-06-03 16:52:41 -0500

    fix: issue #1 - Handle user authentication accordingly to avoid unexpected behaviour

--- lazy_prompts/users/views.py
+++ lazy_prompts/users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import logout
+from django.contrib.auth import authenticate, login, logout
 
 from .forms import UserRegistrationForm, UserLoginForm
 
@@ -19,15 +19,24 @@ def registration_view(request):
 
 
 def my_login(request):
+    if request.method == "GET":
+        form = UserLoginForm()
+        return render(request, "my_login.html", {"form":form})
+
     if request.method == "POST":
         form = UserLoginForm(request.POST)
+
         if form.is_valid():
-            return redirect("feed:home")
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
 
-    else:
-        form = UserLoginForm()
-
-    return render(request, "my_login.html", {"form":form})
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("feed:home")
+            else:
+                form.add_error(None, "Invalid username or password")
+                return render(request, "my_login.html", {"form":form})
 
 
 @login_required
