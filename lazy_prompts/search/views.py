b8295839f7b389722bb5a153ef98325c90a710b5
codinSamueldev <codinsamueldev@gmail.com>
2025-06-23 17:04:07 -0500

    feat: implement search functionality with prompt filtering and error handling

--- lazy_prompts/search/views.py
+++ lazy_prompts/search/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+from prompt_posts.models import Prompt
+
 
 def search_prompts(request):
     """
@@ -8,7 +10,6 @@ def search_prompts(request):
     
     Args:
         request: The HTTP request object.
-        args: The keywords searched by the user.
         
     Returns:
         Rendered search page.
@@ -25,19 +26,29 @@ def search_prompts(request):
     # Determine if the search is by title or prompt.
     selected_search = None
     message = None
+    prompts = None
 
     if by_title:
         selected_search = by_title
         message = "You have searched by title."
+        prompts = Prompt.objects.filter(title__icontains=keywords).distinct()
+        print(prompts, prompts.exists(), prompts.count())
 
     elif by_prompt:
         selected_search = by_prompt
         message = "You have searched by prompt."
+        prompts = Prompt.objects.filter(content__icontains=keywords).distinct()
+
+    
+    # If message is None, or did not found prompts, return a 404.
+    if (not message) or (not prompts.exists()):
+        return JsonResponse({'error': 'Ooops : (, we did not found that.'}, status=404)
 
     context = {
         'keywords': keywords,
         'selected_search': selected_search,
         'message': message,
+        'prompts': prompts,
     }
 
     return render(request, 'search.html', context)
