e1ed680f55c46473fd952e9fcbb6c124ed6ef4a7
codinSamueldev <codinsamueldev@gmail.com>
2025-07-15 17:19:04 -0500

    feat: implement pagination for topic prompts and add pagination controls

--- lazy_prompts/prompt_posts/views.py
+++ lazy_prompts/prompt_posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.template.loader import render_to_string
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 from .forms import PromptCreateForm
 from .models import Prompt, Topic
@@ -37,17 +38,29 @@ def create_prompt(request):
 def topic_prompts(request, slug):
     """Display prompts for a specific topic."""
     try:
-        topic = Topic.objects.get(slug=slug)
+        topic = Topic.objects.prefetch_related(
+            "prompt_set",
+            "prompt_set__author",
+            "prompt_set__topic"
+            ).get(slug=slug)
 
-        if topic.prompt_set.count() >= 1: prompts = topic.prompt_set.select_related('author', 'topic')
+        if topic.prompt_set.count() >= 1: 
+            prompts = topic.prompt_set.all()
+            paginator = Paginator(prompts, 3) # 3 prompts per page.
+
+            page_number = request.GET.get("page")
+            page_obj = paginator.get_page(page_number)
+
         else: prompts = False
+
+        context = {
+            'topic': topic,
+            'prompts': prompts,
+            'form': PromptCreateForm(),
+            'page_obj': page_obj,
+        }
+        
+        return render(request, 'topic_prompts.html', context)
     except Topic.DoesNotExist:
         return JsonResponse({'error': 'Topic not found'}, status=404)
 
-    context = {
-        'topic': topic,
-        'prompts': prompts,
-        'form': PromptCreateForm()
-    }
-    
-    return render(request, 'topic_prompts.html', context)
